df5e9bc7c7bdba7f054f8bd9ee9086c827e1174b
Vlad <vlad05242001@gmail.com>
2021-01-16 13:15:53 +0200

    fix paggination

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-  	@products = Product.paginate(page: params[:page], per_page: 6)
+    @products = Product.paginate(page: params[:page], per_page: 6)
   end
 end
