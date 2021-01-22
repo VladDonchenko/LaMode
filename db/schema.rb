ec5049d3335893477daf58e34b9c75046ede6f41
Vlad <vlad05242001@gmail.com>
2021-01-22 21:11:37 +0200

    AddedImages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_180453) do
+ActiveRecord::Schema.define(version: 2021_01_22_183700) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_180453) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.string "image"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
