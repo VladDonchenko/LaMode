7db622475ce8c6ac6d55cd5f92b3f9105ca832d3
Vlad <vlad05242001@gmail.com>
2021-02-05 20:49:22 +0200

    domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,5 +6,5 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :environment, "production"
 set :rails_env,   "production"
 
-set :nginx_server_name, "207.154.197.143"
+set :nginx_server_name, "tecnoboom.hopto.org"
 set :puma_conf, "#{shared_path}/config/puma.rb"
